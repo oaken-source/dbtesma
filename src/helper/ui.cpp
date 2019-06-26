5a1735e01be1a0158cf937b61a0c9e34e2461ac7
Andreas Grapentin <andreas@grapentin.org>
2019-06-26 11:38:47 +0200

    updated copyright years

--- src/helper/ui.cpp
+++ src/helper/ui.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
  *                          dbtesma data generator                             *
  *                                                                             *
- *    Copyright (C) 2011  Andreas Grapentin                                    *
+ *    Copyright (C) 2011 - 2019  Andreas Grapentin                             *
  *                                                                             *
  *    This program is free software: you can redistribute it and/or modify     *
  *    it under the terms of the GNU General Public License as published by     *
